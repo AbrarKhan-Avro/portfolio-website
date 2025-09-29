a4cbeb0655821a251dbd0803d9179fb6f189a1a4
Abrar Khan <aak.avro@gmail.com>
2025-09-29 20:29:02 +0600

    Initial commit

---
+++ my-portfolio/postcss.config.mjs
@@ -0,0 +1,11 @@
+/*const config = {
+  plugins: ["@tailwindcss/postcss"],
+};
+
+export default config;*/
+// postcss.config.mjs
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
+}
